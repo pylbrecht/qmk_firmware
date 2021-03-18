7b72781f35450ba4e4cd15c3d1cdf7fa3f50b3ad
Philipp Albrecht <philipp.albrecht@momox.biz>
2021-03-18 15:44:37 +0100

    Remove home row mods
    This reverts commit e56688cc1d91431760048c68e9570903e024aca5.

--- keyboards/keebio/iris/keymaps/pylbrecht/keymap.c
+++ keyboards/keebio/iris/keymaps/pylbrecht/keymap.c
@@ -8,12 +8,6 @@
 #define _SLSH CTL_T(KC_SLSH)
 #define _SPC ALT_T(KC_SPC)
 
-// home row mods
-#define CTL_F CTL_T(KC_F)
-#define CTL_J CTL_T(KC_J)
-#define SFT_D SFT_T(KC_D)
-#define SFT_K SFT_T(KC_K)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
@@ -22,9 +16,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_ESC,  KC_A,    KC_S,    SFT_D,   CTL_F,   KC_G,                               KC_H,    CTL_J,   SFT_K,   KC_L,    KC_SCLN, KC_QUOT,
+     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,          _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, _______,
+     KC_LSFT, _Z,      KC_X,    KC_C,    KC_V,    KC_B,    _______,          _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  _SLSH,   KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     MO(_LOWER), _SPC, KC_BSPC,                   KC_ENT,  _SPC,    MO(_LOWER)
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
