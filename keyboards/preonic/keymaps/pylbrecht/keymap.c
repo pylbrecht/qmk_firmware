dacaa34f339cd0ab54e01d70148fd87fbdc1733e
Philipp Albrecht <palbrecht@mailbox.org>
2022-05-01 21:57:43 +0200

    [preonic/rev3] move quote to RAISE

--- keyboards/preonic/keymaps/pylbrecht/keymap.c
+++ keyboards/preonic/keymaps/pylbrecht/keymap.c
@@ -46,7 +46,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
   KC_LSFT, CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH), KC_RSFT,
   OSM(MOD_LCTL | MOD_LSFT), KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RALT, KC_DOWN, KC_UP,   KC_RGHT
 ),
@@ -88,7 +88,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT_preonic_grid(
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, KC_LCBR, KC_RCBR, KC_TILD, _______, _______, _______, _______, _______, _______,
-  _______, _______, _______, KC_LPRN, KC_RPRN, KC_GRV,  _______, _______, _______, _______, _______, _______,
+  _______, _______, KC_QUOT, KC_LPRN, KC_RPRN, KC_GRV,  _______, _______, _______, _______, _______, _______,
   _______, _______, _______, KC_LBRC, KC_RBRC, KC_PIPE, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
