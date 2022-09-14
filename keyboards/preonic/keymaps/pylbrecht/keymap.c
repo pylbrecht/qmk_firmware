bd71cbaf97eeefa225edc5b69531ebb611753312
Philipp Albrecht <palbrecht@mailbox.org>
2022-09-14 21:48:53 +0200

    [preonic/rev3] add home row mod shift

--- keyboards/preonic/keymaps/pylbrecht/keymap.c
+++ keyboards/preonic/keymaps/pylbrecht/keymap.c
@@ -16,6 +16,9 @@
 
 #include QMK_KEYBOARD_H
 
+#define HRM_F SFT_T(KC_F)
+#define HRM_J SFT_T(KC_J)
+
 enum preonic_layers {
   _QWERTY,
   _LOWER,
@@ -46,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-  KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, _______,
-  KC_LSFT, CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH), KC_RSFT,
+  KC_ESC,  KC_A,    KC_S,    KC_D,    HRM_F,    KC_G,    KC_H,    HRM_J,    KC_K,    KC_L,    KC_SCLN, _______,
+  _______, CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH), _______,
   OSM(MOD_LCTL | MOD_LSFT), KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LALT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
