b51939b8f7fa0c2d530dff36a1e8ba989931ba71
pylbrecht <pylbrecht@mailbox.org>
2025-06-06 09:47:50 +0200

    fix: compile errors
    It's been a long time since I compiled this keymap.
    
    IGNORE_MOD_TAP_INTERRUPT is now the default.
    CAPSWRD is now CW_TOGG.

--- keyboards/preonic/keymaps/pylbrecht/keymap.c
+++ keyboards/preonic/keymaps/pylbrecht/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-  KC_ESC,  KC_A,    KC_S,    HRM_D,   HRM_F,   KC_G,    KC_H,    HRM_J,   HRM_K,   KC_L,    KC_SCLN, CAPSWRD,
+  KC_ESC,  KC_A,    KC_S,    HRM_D,   HRM_F,   KC_G,    KC_H,    HRM_J,   HRM_K,   KC_L,    KC_SCLN, CW_TOGG,
   _______, CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH), _______,
   OSM(MOD_LCTL | MOD_LSFT), KC_LCTL, KC_LGUI, _______, LOWER,   KC_SPC,  KC_SPC,  RAISE,   _______, KC_DOWN, KC_UP,   KC_RGHT
 ),
