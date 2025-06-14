05ad7fdaf4ec6152fd7b9ac7004e978a8dfe8e57
pylbrecht <pylbrecht@mailbox.org>
2025-06-14 15:21:25 +0200

    feat: move symbol layer to home row

--- keyboards/preonic/keymaps/pylbrecht/keymap.c
+++ keyboards/preonic/keymaps/pylbrecht/keymap.c
@@ -20,6 +20,8 @@
 #define HRM_J SFT_T(KC_J)
 #define HRM_K RALT_T(KC_K)
 #define HRM_D LALT_T(KC_D)
+#define HRM_S LT(_SYMBOLS, KC_S)
+#define HRM_L LT(_SYMBOLS, KC_L)
 
 enum preonic_layers {
   _QWERTY,
@@ -51,9 +53,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_preonic_grid(
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_ENT,
-  KC_ESC,  KC_A,    KC_S,    HRM_D,   HRM_F,   KC_G,    KC_H,    HRM_J,   HRM_K,   KC_L,    KC_SCLN, CW_TOGG,
+  KC_ESC,  KC_A,    HRM_S,    HRM_D,   HRM_F,   KC_G,    KC_H,    HRM_J,   HRM_K,   HRM_L,    KC_SCLN, CW_TOGG,
   _______, CTL_T(KC_Z),    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  CTL_T(KC_SLSH), _______,
-  OSM(MOD_LCTL | MOD_LSFT), KC_LCTL, KC_LGUI, _______, LOWER,   KC_SPC,  KC_SPC,  RAISE,   _______, KC_DOWN, KC_UP,   KC_RGHT
+  OSM(MOD_LCTL | MOD_LSFT), KC_LCTL, KC_LGUI, _______, LOWER,   KC_SPC,  KC_SPC,  XXXXXXX,   KC_TAB, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Lower
@@ -73,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
   _______, _______, _______, _______, _______, _______, KC_EQL,  KC_7, KC_8, KC_9, KC_ASTR, _______,
   _______, _______, _______, _______, _______, _______, KC_MINS, KC_4, KC_5, KC_6, KC_PLUS, _______,
-  _______, _______, _______, _______, _______, _______, KC_UNDS, KC_1, KC_2, KC_3, KC_BSLS, _______,
+  _______, _______, _______, _______, _______, _______, _______, KC_1, KC_2, KC_3, KC_BSLS, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_0, _______, _______, _______
 ),
 
@@ -91,9 +93,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_SYMBOLS] = LAYOUT_preonic_grid(
-  _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______, _______, _______, _______, _______, _______,
-  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_TILD, _______, _______, _______, _______, _______, _______,
-  _______, KC_QUOT, KC_DQUO, KC_LPRN, KC_RPRN, KC_GRV, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,
+  _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______, _______, _______, _______, _______, KC_DEL,
+  _______, KC_EXLM, KC_AT,   KC_LCBR, KC_RCBR, KC_TILD, _______, _______, _______, KC_UNDS, _______, _______,
+  _______, KC_QUOT, KC_DQUO, KC_LPRN, KC_RPRN, KC_GRV, _______, KC_GT, KC_MINS, KC_EQL, _______, _______,
   _______, KC_PERC, KC_CIRC, KC_LBRC, KC_RBRC, KC_PIPE, _______, _______, _______, _______, _______, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 ),
